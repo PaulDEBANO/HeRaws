2153f70d64ab1849a207818ba65251f86877b7f4
Loelia <loelia579@gmail.com>
2022-04-07 20:12:55 +0200

    Add Material UI (MUI) to the projet + test it on the simulator page

--- src/pages/simulator.js
+++ src/pages/simulator.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+import {Component} from "react";
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Slider from '@mui/material/Slider';
+
+//Palete color 
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#FFC900',
+        main: '#086E7D',
+        dark: '#1A5F7A',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#FFF89A',
+        main: '#FFC900',
+        dark: '#086E7D',
+        contrastText: '#000',
+      },
+    },
+  });
 
 const Simulator = () => {
     return (
@@ -11,6 +35,19 @@ const Simulator = () => {
             }}
         >
             <h1>Simulator</h1>
+
+            <ThemeProvider theme={theme}>
+                <Button color="primary">Primary</Button>
+                <Button color="secondary">Secondary</Button>
+            </ThemeProvider>
+
+            <Slider
+                defaultValue={30}
+                sx={{
+                    width: 300,
+                    color: 'success.main',
+                }}
+            />
         </div>
     );
 };

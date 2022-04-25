f8a4593818925f7f91231d2508c2d98f0ae7e192
Paul Debano <paul.debano@gmail.com>
2022-04-25 10:29:19 +0200

    Add Sign Up Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -49,40 +49,88 @@ export default function BasicModal() {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const [open2, setOpen2] = React.useState(false);
+    const handleOpen2 = () => setOpen2(true);
+    const handleClose2 = () => setOpen2(false);
+
     return (
-        <div>
-            <ColorButton variant="contained" size="large" onClick={handleOpen}>Sign In</ColorButton>
+        <>
+            <div>
+                <ColorButton variant="contained" size="large" onClick={handleOpen}>Sign In</ColorButton>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="Sign In"
+                    aria-describedby="Sign In"
+                >
+                    <Box component='form' autocomplete="off" sx={style} className='xl:w-2/5 md:w-3/5 w-11/12 rounded'>
+                        <Typography id="modal-modal-title" variant="h4" component="h4" align='center'>
+                            Sign In
+                        </Typography>
+                        <Typography id="modal-modal-description" className='mt-9'>
+                            Sign in to access additional content.
+                        </Typography>
+
+                        <div>
+                            <div className='pt-4 pb-4 text-center'>
+                                <TextField fullWidth id="outlined-basic" label="Email" variant="outlined" type="email" autoFocus />
+                            </div>
+                            <div className='text-center'>
+                                <TextField fullWidth id="outlined-basic" label="Password" variant="outlined" type="password"/>
+                            </div>
+                            <p Style="margin-top: 3% ; color: red"><a href=''>Forgot your password ?</a></p>
+                            <div className='mt-9 text-center'>
+                                <ThemeProvider theme={theme_button}>
+                                    <Button color="primary" variant="contained" size='large'>Sign In</Button>
+                                </ThemeProvider>
+                            </div>
+                            <p className='mt-10'>Do not have an account yet ? <Button size='large' onClick={handleOpen2} Style="color: #096F7D"> Sign Up</Button></p>
+                        </div>
+                    </Box>
+                </Modal>
+            </div>
+
             <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="Sign In"
-                aria-describedby="Sign In"
+                open={open2}
+                onClose={handleClose2}
+                aria-labelledby="Sign Up"
+                aria-describedby="Sign Up"
             >
                 <Box component='form' autocomplete="off" sx={style} className='xl:w-2/5 md:w-3/5 w-11/12 rounded'>
-                    <Typography id="modal-modal-title" variant="h4" component="h4" align='center'>
-                        Sign In
+                    <Typography id="modal-modal-title" variant="h3" component="h3" align='center'>
+                        Sign Up
                     </Typography>
-                    <Typography id="modal-modal-description" className='mt-9'>
-                        Connectez-vous pour accéder à du contenu supplémentaire.
-                    </Typography>
-
                     <div>
-                        <div className='pt-4 pb-4 text-center'>
-                            <TextField fullWidth id="outlined-basic" label="Email" variant="outlined" type="email" autoFocus />
+                        <div className='pt-4 pb-12 text-center'>
+                            <Typography id="modal-modal-title" variant="h5" component="h5" align='center'>
+                                Your Name
+                            </Typography>
+                            <TextField id="outlined-basic" label="First name" variant="outlined" type="text" autoFocus />
+                            <TextField id="outlined-basic" label="Last name" variant="outlined" type="text" />
                         </div>
-                        <div className='text-center'>
+                        <div className='pb-12 text-center'>
+                            <Typography id="modal-modal-title" variant="h5" component="h5" align='center'>
+                                Your email
+                            </Typography>
+                            <TextField fullWidth id="outlined-basic" label="Email" variant="outlined" type="email" />
+                        </div>
+                        <div className='pb-4 text-center'>
+                            <Typography id="modal-modal-title" variant="h5" component="h5" align='center'>
+                                Your Password
+                            </Typography>
                             <TextField fullWidth id="outlined-basic" label="Password" variant="outlined" type="password"/>
                         </div>
-                        <p Style="margin-top: 3% ; color: red">Forgot your password ?</p>
+                        <div className='pb-4 text-center'>
+                            <TextField fullWidth id="outlined-basic" label="Confirm Password" variant="outlined" type="password"/>
+                        </div>
                         <div className='mt-9 text-center'>
                             <ThemeProvider theme={theme_button}>
-                                <Button color="primary" variant="contained" size='large'>Sign In</Button>
+                                <Button color="primary" variant="contained" size='large'>Sign Up</Button>
                             </ThemeProvider>
                         </div>
-                        <p className='mt-10'>Do not have an account yet ?<a Style="color: #"> Sign Up</a></p>
                     </div>
                 </Box>
             </Modal>
-        </div>
+        </>
     );
 }
